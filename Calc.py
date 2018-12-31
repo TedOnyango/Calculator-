188b04fc253f050ee1532047eee9ea62d39256af
TedOnyango <45144530+TedOnyango@users.noreply.github.com>
2018-12-31 16:23:33 +0300

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,16 @@
+import math
+import fractions
+operands = ["+", "-", "*" "/"]
+def _addition(x, y):
+    z = x + y
+    return z
+def _subtraction(x, y):
+    z = x + y
+    return z
+def _multiplication(x, y):
+    z = x * y
+    return z
+def _division(x, y):
+    z = x / y
+    return z
+
